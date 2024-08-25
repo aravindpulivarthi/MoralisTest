492321501c7f132c52af43c66fcf02ffdf51da38
Aravind Pulivarthi <aravindpulivarthi@Aravinds-MacBook-Air.local>
2024-08-25 14:35:37 +0200

    Testing Tasks

---
+++ Login_GetBlockDetails.spec.js
@@ -0,0 +1,198 @@
+const {test, expect} = require('@playwright/test')
+require('dotenv').config();
+
+let nodeURL; 
+const username = process.env.USERNAME;
+const password = process.env.PASSWORD;
+let latestBlockNumber;
+let transactionHash;
+
+test.describe('Positive Test cases', ()=>{
+    test('Login to Moralis', async ({page})=>{
+
+        test.setTimeout(75000);
+
+        await page.goto('https://admin.moralis.io/login')
+        await page.waitForTimeout(1000)
+        
+        try {
+            await page.getByRole('button', { name: 'Accept all' }).click();
+            console.log('Cookie consent accepted.');
+        } catch (e) {
+            console.log('Cookie consent dialog not found or failed to click.');
+        }
+
+        await page.fill("//label[@for='admin-login-email']",username)
+        await page.fill("//label[@for='admin-login-password']",password)
+        await page.click("//span[normalize-space()='Log in']")
+        
+        await page.waitForTimeout(5000)
+        await page.click("(//div[@class='_children_1qqmc_425'])[5]")    //Click Nodes
+        await page.click("//button[normalize-space()='Create a New Node']//*[name()='svg']")    //Create a new node
+
+        await page.selectOption("//span[normalize-space()='Select Protocol *']",'Base')     //Select Base protocol
+        await page.waitForTimeout(2000)
+        await page.selectOption("//span[normalize-space()='Select Network *']", 'Mainnet')  //Select Mainnet as Network
+
+        await page.click("//div[@class='DivContainerStyled-sc-1jzr4nq-0 fffdlS']//button[@type='button'][normalize-space()='Create Node']")          //Click create node
+
+        await page.waitForTimeout(3000)
+    
+        console.log('Node created successfully')
+
+        const copyButton = await page.locator("//body[1]/div[1]/div[5]/div[1]/div[1]/main[1]/main[1]/div[1]/div[2]/section[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/span[1]/div[1]/div[1]/button[1]"); 
+        await copyButton.click();
+
+        //Retrieve the copied URL from the clipboard
+        nodeURL = await page.evaluate(() => navigator.clipboard.readText());
+        console.log('Copied Node URL:', nodeURL);
+        
+        
+    });
+
+    
+
+    test('Get Block Number',async ({ request }) => {
+        try{
+            const response = await request.post(nodeURL, {
+            data: {
+                jsonrpc: '2.0',
+                method: 'eth_blockNumber',
+                params: [],
+                id: 1,
+            },
+            });
+
+            expect(response.status()).toBe(200);
+            // Step 2: Get the hex block number from the response
+            const data = await response.json();
+            latestBlockNumber = data.result;
+            console.log("Block Number (Hex):", latestBlockNumber);
+
+            // Step 3: Convert the hex block number to decimal
+            const blockNumberDecimal = parseInt(latestBlockNumber, 16);
+            console.log("Block Number (Decimal):", blockNumberDecimal);
+        }
+        catch {
+            console.error('Failed to fetch transaction details:', error);
+            throw error;
+        }
+    });
+
+    // Step 2: Fetch Block Details Using Latest Block Number
+    test('Fetch block details using latest block number', async ({ request }) => {
+        
+        //Check if we get a block number from the previous test
+        if (latestBlockNumber === undefined) {
+            test.skip('Skipping as latestBlockNumber is not set');
+            return;
+        }
+
+        try{
+            const response = await request.post(nodeURL, {
+            data: {
+                jsonrpc: '2.0',
+                method: 'eth_getBlockByNumber',
+                params: [latestBlockNumber, false], // We don't need full transaction objects
+                id: 1,
+            },
+            });
+
+            expect(response.status()).toBe(200);
+            const data = await response.json();
+            console.log(`Block Details: ${JSON.stringify(data.result)}`);
+            
+            // Assuming the block has at least one transaction, get its hash
+            if (data.result.transactions.length > 0) {
+            transactionHash = data.result.transactions[0];
+            console.log(`Transaction Hash: ${transactionHash}`);
+            }
+            else {
+                throw new Error('No transactions found in the block.');
+            }
+        }
+        catch{
+            console.error('Failed to fetch block details:', error.message);
+            throw error;
+        }
+    });
+
+    // Step 3: Fetch Transaction Details Using Transaction Hash
+    test('Fetch transaction details using transaction hash', async ({ request }) => {
+        // Check if we have a valid transaction hash from the previous test
+        if (!transactionHash) {
+        test.skip('No transaction hash found in the block');
+        }
+        try{
+            const response = await request.post(nodeURL, {
+            data: {
+                jsonrpc: '2.0',
+                method: 'eth_getTransactionByHash',
+                params: [transactionHash],
+                id: 1,
+            },
+            });
+
+            expect(response.status()).toBe(200);
+            const data = await response.json();
+            console.log(`Transaction Details: ${JSON.stringify(data.result)}`);
+            expect(data.result).toHaveProperty('hash', transactionHash);
+        }
+        catch {
+            console.error('Failed to fetch transaction details:', error);
+            throw error;
+        } 
+    }); 
+
+});
+
+//***************Negative scenarios*********//
+
+test.describe('Negative Test Cases', ()=>{
+    //Invalid Login Credentials
+    test('Login with Invalid Credentials', async ({ page }) => {
+        await page.goto('https://admin.moralis.io/login');
+        await page.waitForTimeout(1000);
+        
+        try {
+            await page.getByRole('button', { name: 'Accept all' }).click();
+            console.log('Cookie consent accepted.');
+        } catch (e) {
+            console.log('Cookie consent dialog not found or failed to click.');
+        }
+
+        // Use invalid credentials
+        await page.fill("//label[@for='admin-login-email']", 'aravindpulivarthiv@gmail.com');
+        await page.fill("//label[@for='admin-login-password']", 'sjedhbf2hieh');
+        await page.click("//span[normalize-space()='Log in']");
+        //await page.waitForTimeout(50000);
+        
+        // Check for an error message
+        const errorMessage = await page.locator("//span[@class='sc-DdwlG kQHBdN']").textContent();
+        expect(errorMessage).toContain('Unauthorized');  
+    
+    });
+
+    //Missing Node URL
+    test('Get Block Number with Missing Node URL', async ({ request }) => {
+        try {
+            const response = await request.post('', {
+                data: {
+                    jsonrpc: '2.0',
+                    method: 'eth_blockNumber',
+                    params: [],
+                    id: 1,
+                },
+            });
+            const data = await response.json();
+            expect(data.error).toBeTruthy();
+        } catch (error) {
+            expect(error.message).toContain('Invalid URL');
+        }
+    });
+
+   
+})
+
+
+
---
+++ NFTs_Wallet.spec.js
@@ -0,0 +1,114 @@
+const {test, expect} = require('@playwright/test')
+require('dotenv').config();
+
+let API_Key;
+const username = process.env.USERNAME;
+const password = process.env.PASSWORD;
+test.describe('Positive Tests for getWalletNFTs endpoint',()=>{
+    test('Login to Moralis', async ({page})=>{
+
+        //test.setTimeout(75000);
+
+        await page.goto('https://admin.moralis.io/login')
+        await page.waitForTimeout(3000)
+        
+        try {
+            await page.getByRole('button', { name: 'Accept all' }).click();
+            console.log('Cookie consent accepted.');
+        } catch (e) {
+            console.log('Cookie consent dialog not found or failed to click.');
+        }
+
+        await page.fill("//label[@for='admin-login-email']",username)
+        await page.fill("//label[@for='admin-login-password']",password)
+        await page.click("//span[normalize-space()='Log in']")
+        await page.waitForTimeout(3000)
+        const copyButton = await page.locator("//button[@aria-label='Copy to Clipboard']");
+        await copyButton.click();
+
+        //Retrieve the copied URL from the clipboard
+        API_Key = await page.evaluate(() => navigator.clipboard.readText());
+        console.log('Copied API Key:', API_Key);
+
+    })
+
+    test('Fetch NFTs for a valid wallet address', async ({ request }) => {
+        const response = await request.get('https://deep-index.moralis.io/api/v2/0xff3879b8a363aed92a6eaba8f61f1a96a9ec3c1e/nft', {
+            headers: {
+                'X-API-Key': API_Key,
+            },
+            params: {
+                chain: 'eth', // Specify the blockchain network, e.g., Ethereum
+                limit: 10,      //fetch only 10 NFTs
+            }
+        });
+        expect(response.status()).toBe(200);
+        const nfts = await response.json();
+        console.log(`NFT Response: ${JSON.stringify(nfts.result)}`);
+        expect(nfts).toHaveProperty('result'); // Ensure 'result' field exists
+        //expect(nfts.result.length).toBeGreaterThan(0); // Ensure NFTs are returned
+        expect(nfts.result.length).toBe(10);  //Ensure NFTs are returned exactly 10
+
+
+    });
+});
+
+//*********Negative Scenarios******/
+test.describe('Positive Tests for getWalletNFTs endpoint',()=>{
+    //Invalid API Key
+    test('Fetch NFTs with invalid API Key', async ({ request }) => {
+        const invalidApiKey = 'invalid_api_key';
+        
+        const response = await request.get('https://deep-index.moralis.io/api/v2/0xff3879b8a363aed92a6eaba8f61f1a96a9ec3c1e/nft', {
+            headers: {
+                'X-API-Key': invalidApiKey,
+            },
+            params: {
+                chain: 'eth',
+                limit: 10,
+            }
+        });
+
+        expect(response.status()).toBe(401); // Unauthorized
+        const responseBody = await response.text();
+        console.log('Error response:', responseBody);
+        
+    });
+
+
+    //
+    test('Fetch NFTs with invalid wallet address', async ({ request }) => {
+        const response = await request.get('https://deep-index.moralis.io/api/v2/invalid_wallet_address/nft', {
+            headers: {
+                'X-API-Key': API_Key,
+            },
+            params: {
+                chain: 'eth',
+                limit: 10,
+            }
+        });
+
+        expect(response.status()).toBe(400); // Bad Request
+        const responseBody = await response.text();
+        console.log('Error response:', responseBody);
+    
+    });
+
+    //Invalid Chain Parameter
+    test('Fetch NFTs with incorrect chain parameter', async ({ request }) => {
+        const response = await request.get('https://deep-index.moralis.io/api/v2/0xff3879b8a363aed92a6eaba8f61f1a96a9ec3c1e/nft', {
+            headers: {
+                'X-API-Key': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6ImM5YmE3YWY2LWNlYTctNDhmZi04MWZkLWFjZGEzZWIzY2I5NiIsIm9yZ0lkIjoiNDA1NDI1IiwidXNlcklkIjoiNDE2NTk5IiwidHlwZUlkIjoiNzA3ODdiZTgtMmEyZi00ZjMwLWI5MDYtNzNlODVhN2NmM2RkIiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE3MjQyMzU4NjksImV4cCI6NDg3OTk5NTg2OX0.23mk1MiOw0VVFhPijskItFQFUiNlB_ipzbRN4mg3KVY',
+            },
+            params: {
+                chain: 'invalid_chain',
+                limit: 10,
+            }
+        });
+
+        expect(response.status()).toBe(400); // Bad Request
+        const responseBody = await response.text();
+        console.log('Error response:', responseBody);
+        
+    });
+});
